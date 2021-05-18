5910a07eb7aae79eca82c44ea850ad9313ce6850
KetanPaliwal <84372503+KetanPaliwal@users.noreply.github.com>
2021-05-18 22:36:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 const {PeerServer} = require("peer");
-PeerServer({port: 443,path: "/myapp",secure: true});
+PeerServer({port: 3001,path: "/myapp",secure: true});
 
 const express = require("express");
 const app = express();
